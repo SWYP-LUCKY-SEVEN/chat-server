5c3ea52983ea9cb47779517093d1c9d577552d2f
Koak DoHun <58322578+dori2005@users.noreply.github.com>
2024-08-07 14:33:39 +0900

    refactor: redis User 연결 제거

--- src/routes/userRoutes.ts
+++ src/routes/userRoutes.ts
@@ -7,7 +7,6 @@ router.get("/", protect, userController.getUsers);
 router.post("/", userController.createUser);
 router.delete("/:id", userController.deleteUser);
 router.patch("/:id", userController.updateUser);
-router.get("/save-key", userController.getUserKey);
 router.post("/sign-up", userController.signUpUser);
 router.post("/sign-in", userController.signInUser);
 
