a1d7e0d7c4aeaab483311973ce3c5dd5d6a98188
Dori <58322578+dori2005@users.noreply.github.com>
2024-10-27 22:20:10 +0900

    fix: redis 미사용

--- src/models/interfaces/IMessage.ts
+++ src/models/interfaces/IMessage.ts
@@ -3,6 +3,7 @@ import Chat from "./IChat";
 import User from "./IUser";
 
 export interface IMessage {
+  index: number;
   sender?: string;
   isPic: boolean;
   content?: string;
--- src/services/messageService.ts
+++ src/services/messageService.ts
@@ -35,48 +35,33 @@ const getAllMessages = async (chatId: string) => {
   }
 };
 
+// 최신 메세지 redis 사용 안할 듯 함.
 const getRecentMessages = async (chatId: string, page:number, limit: number) => {
   if (!chatId) {
     const error = new Error("chatId 확인") as IError;
     error.statusCode = 400;
     throw error;
-  } else {
-    console.log(`lrange room:${chatId} 0 -1`);
-    const messages = await redisClient.lRange(`room:${chatId}`, 0, -1);
-    console.log(messages);
-
-    let resultMessages;
-    if(messages) {
-      const parsedMessages = messages.map(message => JSON.parse(message));
-      if(messages.length < limit){
-        const loadMessages = await Message.find({ chat: chatId })
-          .sort({ timestamp: -1 })
-          .skip(messages.length)
-          .limit(limit - messages.length)
-          .populate("sender", "nickname pic email");
-        resultMessages = parsedMessages.concat(loadMessages);
-      }
-    } else {
-      const loadMessages = await Message.find({ chat: chatId })
-        .sort({ timestamp: -1 })
-        .limit(limit)
-        .populate("sender", "nickname pic email");
-      resultMessages = loadMessages;
-    }
-
-    const chat = await Chat.findById(chatId, { noti:0 });
-    const data = {
-      chat : chat,
-      messages : resultMessages
-    }
-
-    if (resultMessages && chat) return data;
-    else {
-      const error = new Error("메시지 조회 실패") as IError;
-      error.statusCode = 404;
-      throw error;
-    }
   }
+  const messages = await redisClient.lRange(`room:${chatId}`, 0, -1);
+
+  let resultMessages = await Message.find({ chat: chatId })
+    .sort({ timestamp: -1 })
+    .skip(page*limit)
+    .limit(limit)
+    .populate("sender", "nickname pic email");
+
+  const chat = await Chat.findById(chatId, { noti:0 });
+  const data = {
+    chat : chat,
+    messages : resultMessages
+  }
+
+  if (resultMessages && chat) return data;
+  else {
+    const error = new Error("메시지 조회 실패") as IError;
+    error.statusCode = 404;
+    throw error;
+  }  
 };
 
 
@@ -118,31 +103,6 @@ const sendMessage = async (
   let message:any = await Message.create(newMessage);
   message = await message.populate("sender", "nickname pic");
 
-  const cacheData = {
-    sender : message.sender,
-    content,
-    createdAt : message.createdAt
-  }
-
-  try {
-    const lastMessage = await redisClient.lRange(`room:${chatId}`, 0, 0); //최근 메시지와 비교
-    if(lastMessage){
-      const lastM:any = JSON.parse(lastMessage[0]);
-      const lastMTime = lastM.createdAt;
-      const timeDifference = message.createdAt - lastMTime; //Date 간의 연산은 밀리초 단위로 계산됨.
-      if (timeDifference >= EXPIRY_TIME_THRESHOLD) {
-          await redisClient.expire(`room:${chatId}`, TTL_SECONDS);
-      }
-    } else {
-      await redisClient.expire(`room:${chatId}`, TTL_SECONDS);
-    }
-    const result = await redisClient.lPush(`room:${chatId}`, JSON.stringify(cacheData));
-    console.log(`Message added to room:${chatId}:`, result);
-    await redisClient.LTRIM(`room:${chatId}`, 0, 29);
-  } catch (err) {
-      console.error('Error:', err);
-  }
-
   if (message) {
     await Chat.findByIdAndUpdate(chatId, { latestMessage: message });
     return message;
@@ -152,6 +112,25 @@ const sendMessage = async (
     throw error;
   }
 };
+
+const findMessageText = async (
+  findText: string,
+  chatId: string,
+  LoadStartIndex: Number
+) => {
+  if (!findText || !chatId) {
+    const error = new Error("유효하지 않은 요청") as IError;
+    error.statusCode = 400;
+    throw error;
+  }
+  
+  const message = await Message.find(
+    { 
+      content:findText 
+    }
+  )
+
+}
 
 const sendPicture = async (
   content: string,
