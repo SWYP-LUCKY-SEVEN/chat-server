a4148bebf68e2645698a62d44c99a645dfb6182b
Koak DoHun <58322578+dori2005@users.noreply.github.com>
2024-12-15 13:50:28 +0900

    fix: message path param 오류 수정

--- src/routes/messageRoutes.ts
+++ src/routes/messageRoutes.ts
@@ -4,11 +4,11 @@ import { protect } from "@middlewares/authMiddleware";
 
 const router = express.Router();
 
-router.route("/:chatId").get(protect, messageController.getAllMessages);
-router.route("/:chatId/recent").get(protect, messageController.getRecentMessages);
-router.route("/:chatId/range").get(protect, messageController.getMessagesByRange);
-router.route("/:chatId/text").get(protect, messageController.findMessageByText);
-router.route("/:chatId/index").get(protect, messageController.findMessagesBetweenIndex);
+router.route("/:studyId").get(protect, messageController.getAllMessages);
+router.route("/:studyId/recent").get(protect, messageController.getRecentMessages);
+router.route("/:studyId/range").get(protect, messageController.getMessagesByRange);
+router.route("/:studyId/text").get(protect, messageController.findMessageByText);
+router.route("/:studyId/index").get(protect, messageController.findMessagesBetweenIndex);
 
 router.route("/").post(protect, messageController.sendMessage);
 
